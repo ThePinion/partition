0ae7589790605aceb87b6754e54a901581c14879
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-05-12 09:18:51 +0000

    2d characteristic

--- src/characteristic.rs
+++ src/characteristic.rs
@@ -4,9 +4,9 @@ impl Characteristic {
     pub fn with_size_1d(size: usize) -> Characteristic1d {
         Characteristic1d { size }
     }
-    // pub fn with_size_2d(x_size: usize, y_size: usize) -> Characteristic2d {
-    //     Characteristic2d { x_size, y_size }
-    // }
+    pub fn with_size_2d(x_size: usize, y_size: usize) -> Characteristic2d {
+        Characteristic2d { x_size, y_size }
+    }
 }
 
 pub trait CharacteristicTrait<T> {
@@ -38,35 +38,49 @@ impl<'a> CharacteristicTrait<u64> for &'a Characteristic1d {
     }
 }
 
-// pub struct Characteristic2d {
-//     x_size: usize,
-//     y_size: usize,
-// }
-// impl<'a> CharacteristicTrait<(u64, u64)> for &'a Characteristic2d {
-//     fn encode(self, value: &[(u64, u64)]) -> Vec<bool> {
-//         todo!()
-//     }
+pub struct Characteristic2d {
+    x_size: usize,
+    y_size: usize,
+}
+impl<'a> CharacteristicTrait<(u64, u64)> for &'a Characteristic2d {
+    fn encode(self, value: &[(u64, u64)]) -> Vec<bool> {
+        let mut encoded = vec![false; self.x_size * self.y_size];
+        for (x, y) in value {
+            encoded[(x * self.y_size as u64 + y) as usize] = true;
+        }
+        encoded
+    }
 
-//     fn decode(self, characteristic: &[bool]) -> Vec<(u64, u64)> {
-//         todo!()
-//     }
-// }
+    fn decode(self, characteristic: &[bool]) -> Vec<(u64, u64)> {
+        let mut out = vec![];
+        for (i, val) in characteristic.iter().enumerate() {
+            if *val {
+                out.push((i as u64 / self.y_size as u64, i as u64 % self.y_size as u64));
+            }
+        }
+        out
+    }
+}
 
-// fn characteristic_vector_2d(a: &[(u64, u64)]) -> Vec<bool> {
-//     let max_x = a.iter().map(|(x, _)| x).max().unwrap_or(&0u64);
-//     let max_y = a.iter().map(|(_, y)| y).max().unwrap_or(&0u64);
-//     for i in a {
-//         v[*i as usize] = true;
-//     }
-//     v
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// fn characteristic_vector_2d_inverse(a: &[bool]) -> Vec<u64> {
-//     let mut out = vec![];
-//     for i in 0..a.len() {
-//         if a[i] {
-//             out.push(i as u64);
-//         }
-//     }
-//     out
-// }
+    #[test]
+    fn test_encode_decode_1d() {
+        let characteristic = Characteristic::with_size_1d(10);
+        let value = vec![1, 3, 5, 7, 9];
+        let encoded = characteristic.encode(&value);
+        let decoded = characteristic.decode(&encoded);
+        assert_eq!(value, decoded);
+    }
+
+    #[test]
+    fn test_encode_decode_2d() {
+        let characteristic = Characteristic::with_size_2d(3, 4);
+        let value = vec![(0, 0), (1, 1), (2, 2), (2, 3)];
+        let encoded = characteristic.encode(&value);
+        let decoded = characteristic.decode(&encoded);
+        assert_eq!(value, decoded);
+    }
+}
